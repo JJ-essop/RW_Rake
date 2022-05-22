494c5b2b04cfcdb79045b6028095d73cd5cdc046
JJ <joseph.jessop@uk.alpineracing.com>
2022-05-22 22:14:16 +0100

    Exports data for entire map

--- main.py
+++ main.py
@@ -19,28 +19,42 @@ def read_tap_map():
         r'G:\01 - Aero Projects\15 - WT Data\WTRakes\RW Onset Flow Rake\RW_Tap_Map.csv')
     return tap_map
 
+def read_clio_table():
+    clio_table = pd.read_csv(
+        r'G:\01 - Aero Projects\15 - WT Data\WTRakes\RW Onset Flow Rake\22C_v3.01_Clio_table.csv', usecols=[1, 2, 5, 6, 7])
+    return clio_table
+
 
 CLIO_API_BASE_URL = "http://en-cliows-prod/api"
+run_no = 104612
 
-result = get_run_pressure_data(104612)
+result = get_run_pressure_data(run_no)
 
 tap_map = read_tap_map()
+clio_table = read_clio_table()
 
-Cp = []
-for channel in tap_map.CHANNEL:
-    Cp.append(result[3][channel])
+set_points = list(range(len(result)))
 
-output = pd.DataFrame([
-    list(tap_map.X),
-    list(tap_map.Y),
-    list(tap_map.Z),
-    Cp
-])
+for sp in set_points:
+    Cp = []
 
-output = np.transpose(output)
-output.columns = ["X", "Y", "Z", "TotalPressureCoefficient"]
-output.head()
+    for channel in tap_map.CHANNEL:
+        Cp.append(result[sp][channel])
 
-print(output)
 
-output.to_csv(r"G:\01 - Aero Projects\15 - WT Data\WTRakes\RW Onset Flow Rake\run_no.csv", sep=",", index=False)
+    output = pd.DataFrame([
+        list(tap_map.X),
+        list(tap_map.Y),
+        list(tap_map.Z),
+        Cp
+    ])
+
+    output = np.transpose(output)
+    output.columns = ["X", "Y", "Z", "TotalPressureCoefficient"]
+    output.head()
+
+    filename = f"FRH_{clio_table.Frh_tgt[sp]}__RRH_{clio_table.Rrh_tgt[sp]}__Yaw_{clio_table.Yaw_tgt[sp]}__Steer_{clio_table.Steer_tgt[sp]}__Roll_{clio_table.Roll_tgt[sp]}"
+
+    print(filename)
+
+    output.to_csv(r"G:\01 - Aero Projects\15 - WT Data\WTRakes\RW Onset Flow Rake\WT Results\22W18\JJ\R109080\%s.csv" % filename, sep=",", index=False)
