01c31e50425528d1867c700e389d1d83cc484d78
JJ <joseph.jessop@uk.alpineracing.com>
2022-06-03 12:19:10 +0100

    Restructured and commented

--- functions.py
+++ functions.py
@@ -1,6 +1,8 @@
 import requests
 import pandas as pd
+import numpy as np
 
+# API for extracting oressure coefficient data from a given run
 def get_run_pressure_data(run_id):
     CLIO_API_BASE_URL = "http://en-cliows-prod/api"
     url = f"{CLIO_API_BASE_URL}/pressurecoefficient/{run_id}"
@@ -8,6 +10,7 @@ def get_run_pressure_data(run_id):
     return api_out
 
 
+# Checks to see if API call was successful
 def query_api(url, timeout=30, **kwargs):
     response = requests.get(url, timeout=timeout, **kwargs)
     if response.ok:
@@ -15,14 +18,50 @@ def query_api(url, timeout=30, **kwargs):
     response.raise_for_status()
 
 
+# Returns the tapping map for a given rake - currently hard coded
 def read_tap_map():
     tap_map = pd.read_csv(
         r'G:\01 - Aero Projects\15 - WT Data\WTRakes\RW Onset Flow Rake\RW_Tap_Map.csv')
     return tap_map
 
 
+# Creates a data frame of X, Y, Z locations of pressure probes and teh Cp value for each probe
+# Iterates through the API output of all pressure channels and takes the Cp value for each channel specified in the tapping map
+def generate_Cp_dataframe(result, tap_map):
+    for sp in list(range(len(result))):
+        Cp = []
+
+        for channel in tap_map.CHANNEL:
+            Cp.append(result[sp][channel])
+
+        output = pd.DataFrame([
+            list(tap_map.X),
+            list(tap_map.Y),
+            list(tap_map.Z),
+            Cp
+        ])
+
+        output = np.transpose(output)
+        output.columns = ["X", "Y", "Z", "TotalPressureCoefficient"]
+        output.head()
+
+        return output
+
+
+# Returns the clio table for the given map in order to get car conditions - currently hard coded until API is updated
 def read_clio_table():
     clio_table = pd.read_csv(
         r'G:\01 - Aero Projects\15 - WT Data\WTRakes\RW Onset Flow Rake\22D_v4.01_Clio_table.csv',
         usecols=[1, 2, 5, 6, 7])
     return clio_table
+
+
+# Exports Cp data frame to csv file for each set point, using the clio table for file naming convention
+def export_to_csv(result, clio_table, output):
+    for sp in list(range(len(result))):                     # iterate through all set points in the map
+        filename = f"FRH_{clio_table.Frh_tgt[sp]}__RRH_{clio_table.Rrh_tgt[sp]}__Yaw_{clio_table.Yaw_tgt[sp]}__Steer_{clio_table.Steer_tgt[sp]}__Roll_{clio_table.Roll_tgt[sp]}"
+        print(filename)
+
+        output.to_csv(
+            r"G:\01 - Aero Projects\15 - WT Data\WTRakes\RW Onset Flow Rake\WT Results\22W20\R109418\%s.csv" % filename,
+            sep=",", index=False)
--- main.py
+++ main.py
@@ -1,46 +1,13 @@
-from functions import get_run_pressure_data, read_tap_map, read_clio_table
-import pandas as pd
-import numpy as np
-
-def export_to_csv():
-    set_points = list(range(len(result)))
-
-    for sp in set_points:
-        Cp = []
-
-        for channel in tap_map.CHANNEL:
-            Cp.append(result[sp][channel])
-
-        output = pd.DataFrame([
-            list(tap_map.X),
-            list(tap_map.Y),
-            list(tap_map.Z),
-            Cp
-        ])
-
-        output = np.transpose(output)
-        output.columns = ["X", "Y", "Z", "TotalPressureCoefficient"]
-        output.head()
-
-        filename = f"FRH_{clio_table.Frh_tgt[sp]}__RRH_{clio_table.Rrh_tgt[sp]}__Yaw_{clio_table.Yaw_tgt[sp]}__Steer_{clio_table.Steer_tgt[sp]}__Roll_{clio_table.Roll_tgt[sp]}"
-
-        print(filename)
-
-        # output.to_csv(
-        #     r"G:\01 - Aero Projects\15 - WT Data\WTRakes\RW Onset Flow Rake\WT Results\22W20\R109418\%s.csv" % filename,
-        #     sep=",", index=False)
-
+from functions import get_run_pressure_data, read_tap_map, read_clio_table, generate_Cp_dataframe, export_to_csv
 
 # run_no = 104950
 run_no = input('Enter Clio RunID: ')
 
 result = get_run_pressure_data(run_no)
 
-print(len(result))
-
 tap_map = read_tap_map()
 clio_table = read_clio_table()
 
-print(len(clio_table))
+output = generate_Cp_dataframe(result, tap_map)
 
-export_to_csv()
+export_to_csv(result, clio_table, output)
