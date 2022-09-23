5edc1b5787f242207efcd954543dbbc986b80fb0
JJ <joseph.jessop@uk.alpineracing.com>
2022-09-23 11:16:51 +0100

    Tidy up in new branch

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from functions import get_run_pressure_data, read_tap_map, generate_Cp_dataframe
-#export_to_csv
 
 # run_no = 104950
 run_no = input('Enter Clio RunID: ')
@@ -10,5 +9,3 @@ print(result[15])
 tap_map = read_tap_map()
 
 generate_Cp_dataframe(result, tap_map)
-
-#export_to_csv(result, output)
